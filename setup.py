96132231f09b5ff7433a460cf0e619d19793b286
Sergei Pikhovkin <pikhovkin@gmail.com>
2025-12-26 05:01:47 +0300

    Init

--- setup.py
+++ setup.py
@@ -17,6 +17,9 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.12'
+        'Programming Language :: Python :: 3.13'
+        'Programming Language :: Python :: 3.14'
+        'Programming Language :: Python :: 3.15'
     ],
     keywords=[
         'xtea3',
